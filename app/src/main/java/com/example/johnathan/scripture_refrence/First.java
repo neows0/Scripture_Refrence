c387a0823e2f3ae87aba43e21b222a5c3af1c551
Johnathan Rudy <Johnathan Rudy>
2016-05-20 20:52:17 -0600

    Now it accepts user input but does nothing with it

--- app/src/main/java/com/example/johnathan/scripture_refrence/First.java
+++ app/src/main/java/com/example/johnathan/scripture_refrence/First.java
@@ -3,9 +3,12 @@ package com.example.johnathan.scripture_refrence;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class First extends AppCompatActivity {
 
@@ -24,16 +27,36 @@ public class First extends AppCompatActivity {
     }
 
     private EditText bookName;
+    private EditText chap;
+    private EditText verse;
 
-    public void buttonOnClick(View v){
+    public void but2(View v){
         Button button = (Button) v;
         bookName = (EditText)findViewById(R.id.Book);
+
+        //Toast toast = new Toast(getApplicationContext());
+        //toast.setGravity(Gravity.TOP| Gravity.LEFT, 0, 0);
+        //toast.makeText(First.this, bookName.getText(), toast.LENGTH_SHORT).show();
+
     }
+
+    public void but3(View v) {
+        Button button = (Button) v;
+        chap = (EditText) findViewById(R.id.Chapter);
+    }
+
+    public void but4(View v) {
+        Button button = (Button) v;
+        verse = (EditText) findViewById(R.id.Verse);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        //pBook = (TextView)findViewById(R.id.Book);
+        //pBook.setText(bookName.getText().toString());
         init();
     }
 }
